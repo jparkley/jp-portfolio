105d8ddf46001717abfbfd7ebee0d44a8b1f286a
jparkley <parkleydondon@yahoo.com>
2024-05-03 22:58:36 -0700

    add content and icons

--- src/components/LeftMenu.tsx
+++ src/components/LeftMenu.tsx
@@ -1,18 +1,20 @@
+import { AiFillLinkedin, AiFillGithub } from "react-icons/ai";
+
 const LeftMenu = () => {
   return (
     <>
-      <div className="mb-5">
+      <div className="mb-5 text-center">
         <h1 className="text-4xl font-extrabold">Janelle Parklee</h1>
       </div>
-      <div className="mb-5">
+      <div className="mb-5 text-center">
         <h4 className="text-xl font-bold">Senior Full Stack Engineer</h4>
       </div>
       <div>
-        <p className="text-sm font-light decoration-1">
+        <p className="mb-20 text-center text-sm font-light">
           Driving Excellence with Every Line of Code
         </p>
       </div>
-      <div className="mt-20 p-4 flex justify-right">
+      <div className="flex justify-center mb-10">
         <div>
           <img
             className="block"
@@ -38,10 +40,9 @@ const LeftMenu = () => {
           />
         </div>
       </div>
-      <div className="flex justify-right mt-8">
-        <div>
-          <p className="text-sm">Github | LinkedIn | More</p>
-        </div>
+      <div className="flex justify-center pl-2">
+        <AiFillLinkedin size={42} className="mr-4" />
+        <AiFillGithub size={42} />
       </div>
     </>
   );
